1bc40aa17f03fe894702c1e49c84f12a963746fa
Aman <119519019+AmanDagar144@users.noreply.github.com>
2024-10-10 18:24:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,43 +1,35 @@
 //{ Driver Code Starts
-// Initial template for Java
+// Initial Template for Java
 
 import java.io.*;
+import java.lang.*;
 import java.util.*;
 
-public class GFG {
+class Driver {
     public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-            Solution ob = new Solution();
 
-            System.out.println(ob.revStr(s));
+        while (t-- > 0) {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseString(str));
         }
     }
 }
 // } Driver Code Ends
 
 
-class Solution {
-    static String revStr(String s) {
-        // code here
-        StringBuilder sb=new StringBuilder(s);
-        return sb.reverse().toString();
-    // String a="";
-    // for(int i=s.length()-1;i>=0;i--){
-    //     a=a+s.charAt(i);
-    // }
-    // return a;
-        
-        // for(int i=0;i<sb.length()/2;i++){
-        //     int f=i;
-        //     int b=sb.length()-1-i;
-        //     char x=sb.charAt(f);
-        //     char c=sb.charAt(b);
-        //     sb.setCharAt(f,c);
-        //     sb.setCharAt(b,x);
-        // }
-        // return s;
+// User function Template for Java
+
+class Reverse {
+    // Complete the function
+    // str: input string
+    public static String reverseString(String s) {
+        // Reverse the string str
+        String a="";
+        for(int i=s.length()-1;i>=0;i--){
+            a=a+s.charAt(i);
+        }
+        return a;
     }
 }
