1fa193998e290b9edee3513d7802195d18235e05
Aman <119519019+AmanDagar144@users.noreply.github.com>
2024-11-05 19:02:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/logical Operators - Java/logical-operators-java.java
+++ Difficulty: Easy/logical Operators - Java/logical-operators-java.java
@@ -13,7 +13,7 @@ import java.util.*;
 class Geeks{
     
     static void logicOp(boolean a, boolean b){
-      System.out.print((a&&b)+" "+(a||b)+" "+((!a)&&(!b)));
+     System.out.print((a&&b)+" "+(a||b)+" "+((!a)&&(!b)));
         /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces*/
     }
 }
@@ -42,7 +42,9 @@ class GFG {
 		    //and passing a,b as arguments
 		    g.logicOp(a, b);
 		    System.out.println();
-		}
+		
+System.out.println("~");
+}
 	}
 }
 // } Driver Code Ends
