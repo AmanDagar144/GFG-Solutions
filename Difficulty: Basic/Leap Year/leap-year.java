b0d7bbef5a7098d5dfdb40366a57e395333ad560
Aman <119519019+AmanDagar144@users.noreply.github.com>
2024-11-06 18:41:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Leap Year/leap-year.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isLeap(N));
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int isLeap(int n){
+        //code here
+        if(n %4 ==0 && n %100 !=0 || n %400 ==0){
+            return 1;
+        }else{
+            return 0;
+        }
+        
+    }
+}
