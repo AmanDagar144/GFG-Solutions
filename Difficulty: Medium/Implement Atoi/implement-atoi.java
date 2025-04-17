8fabb62541b6b790b8decd4c719b7289f67f08cb
Aman <119519019+AmanDagar144@users.noreply.github.com>
2025-04-17 13:35:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Atoi/implement-atoi.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial template for JAVA
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t > 0) {
+            String str = sc.nextLine();
+
+            Solution obj = new Solution();
+            int num = obj.myAtoi(str);
+            System.out.println(num);
+            System.out.println("~");
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int myAtoi(String s) {
+        // Your code here
+        int i = 0, n = s.length();
+        int sign = 1;
+        long result = 0;
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            result = result * 10 + digit;
+
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int)(sign * result);
+    }
+}
