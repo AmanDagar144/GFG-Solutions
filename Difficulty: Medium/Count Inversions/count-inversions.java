6ad63387001a761bd664154e9c42e718a1732a52
Aman <119519019+AmanDagar144@users.noreply.github.com>
2025-04-30 21:49:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().inversionCount(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to count inversions in the array.
+    static int inversionCount(int arr[]) {
+        // Your Code Here
+        return mergeSort(arr, new int[arr.length], 0, arr.length - 1);
+    }
+
+    private static int mergeSort(int[] arr, int[] temp, int left, int right) {
+        int mid, invCount = 0;
+        if (right > left) {
+            mid = (left + right) / 2;
+            invCount += mergeSort(arr, temp, left, mid);
+            invCount += mergeSort(arr, temp, mid + 1, right);
+            invCount += merge(arr, temp, left, mid + 1, right);
+        }
+        return invCount;
+    }
+
+    private static int merge(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left, j = mid, k = left, invCount = 0;
+        while (i <= mid - 1 && j <= right) {
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else {
+                temp[k++] = arr[j++];
+                invCount += (mid - i);
+            }
+        }
+        while (i <= mid - 1)
+            temp[k++] = arr[i++];
+        while (j <= right)
+            temp[k++] = arr[j++];
+        for (i = left; i <= right; i++)
+            arr[i] = temp[i];
+        return invCount;
+        
+    }
+}
