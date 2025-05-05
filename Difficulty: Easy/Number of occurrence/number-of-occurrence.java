fa99ce4dcf275057c03c70813455541f462d88ed
Aman <119519019+AmanDagar144@users.noreply.github.com>
2025-05-05 21:44:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.countFreq(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int first = findFirst(arr, target);
+        if (first == -1) {
+            return 0; // target not found
+        }
+        int last = findLast(arr, target);
+        return last - first + 1;
+    }
+
+    private static int findFirst(int[] arr, int target) {
+        int low = 0, high = arr.length - 1, result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                high = mid - 1; // look on left side
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    private static int findLast(int[] arr, int target) {
+        int low = 0, high = arr.length - 1, result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                low = mid + 1; // look on right side
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+}
