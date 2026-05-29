4c7a0fdb4371b8f3c62b6225df05acc3434bff5d
Aman <119519019+AmanDagar144@users.noreply.github.com>
2026-05-29 21:43:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Mean of a Numpy Array/find-the-mean-of-a-numpy-array.py
@@ -0,0 +1,10 @@
+def mean_of_elements(arr):
+    # Code here
+    # mean me avg nikalna h
+    avg = 0
+    n = len(arr)
+    for i in arr:
+        avg = avg + i 
+    avg = avg/n
+    return avg
+    
