bdacc87ffa8f6d5b5b46dfd681d2f5b46cc5ceff
Aman <119519019+AmanDagar144@users.noreply.github.com>
2026-06-11 10:43:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.py
+++ Difficulty: Basic/Reverse a String/reverse-a-string.py
@@ -3,9 +3,5 @@
 class Solution:
      def reverseString(self, s: str) -> str:
         # code here
-        rev=" "
-        for i in s:
-            rev = i+rev
-        return rev 
-        
+        return(s[::-1])
         
