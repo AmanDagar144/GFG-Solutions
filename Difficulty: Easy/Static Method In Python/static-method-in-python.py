16891bda27ef58c68730396b6977cff4cd90fda2
Aman <119519019+AmanDagar144@users.noreply.github.com>
2026-06-14 13:43:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Static Method In Python/static-method-in-python.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+# Implement Addition class here
+class Addition:
+    # def __init__(self,a(int),b(int)):
+    #     self.a = a
+    #     self.b = b
+        @staticmethod
+        def add(a:int,b:int):
+            sum = a+b
+            return sum
